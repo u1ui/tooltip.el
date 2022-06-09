45e03353fc424ec4e374a1ab53f777c2dd82a9ca
Tobias Buschor <tobias.buschor@shwups.ch>
2022-06-09 22:17:07 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tooltip.el
 Tooltips
+
+
+## Ussage
+```html
+<button>
+    copy
+    <u1-tooltip>to clipboard</u1-tooltip>
+</button>
+```	
+
+### Note
+Dont style the tooltip using anchestor selector! The tooltip element will move to the body element. Use classes or the style-attribute.
+
+```html
+<u1-tooltip class=myToolTip>copy</u1-tooltip>
+```
--- tooltip.js
+++ tooltip.js
@@ -2,68 +2,64 @@
 let idCounter = 0;
 
 customElements.define('u1-tooltip', class extends HTMLElement {
-
     constructor() {
         super();
     }
-
     connectedCallback() {
-
         if (!this.id) { // if no id is set, set one an make it the tooltip for its parent
             this.id = 'u1-tooltip-' + idCounter++;
             this.parentNode.setAttribute('aria-labeledby', this.id);
         }
 
-        //this.targets = document.querySelectorAll('aria-labeledby['+this.id+'], aria-describedby['+this.id+']');
+        const rootEl = document.body;
+        this.parentNode !== rootEl && rootEl.append(this);
     }
-
-    disconnectedCallback() {
-    }
-
     showFor(el){
-        document.body.append(this);
+
+        let event = new CustomEvent('u1-tooltip-show', {
+            bubbles: true,
+            cancelable: true,
+            detail: {for: el}
+        });
+        this.dispatchEvent(event);
+        if (event.defaultPrevented) return;
+
+        this.style.willChange = 'opacity, visibility';
+        this.setAttribute('open','');
+
+        // bottom
+        // todo z-index top
+        this.style.marginLeft = '0';
+        this.style.marginRight = '0';
+        this.style.marginBottom = '0';
         const rect = el.getBoundingClientRect();
         const elCenter = rect.left + rect.width / 2;
-        this.style.display = 'block';
         this.style.left = elCenter - this.offsetWidth/2 + scrollX + 'px';
-        this.style.top = rect.bottom + scrollY + 'px';
-        this.style.transition = 'none';
-        this.style.opacity = '0';
-        clearTimeout(this._hideTimeout);
-        setTimeout(()=>{ // Wait a moment, maybe the user is just running over for a moment
-            this.style.opacity = '1';
-            this.style.transition = ''; // transition back to normal
-        },150);
+        this.style.top  = rect.bottom + scrollY + 'px';
     }
     hide(){
-        this.style.opacity = '0';
-        clearTimeout(this._hideTimeout);
-        this._hideTimeout = setTimeout(()=>{
-            this.style.display = 'none';
-        },1000);
+        this.removeAttribute('open');
     }
-
-
 });
 
-addEventListener('mouseenter',checkOn,true);
+
+document.addEventListener('mouseenter',checkOn,true); // on document: chrome
 addEventListener('focusin',checkOn,true);
-addEventListener('mouseleave',checkOff,true);
+document.addEventListener('mouseleave',checkOff,true);
 addEventListener('focusout',checkOff,true);
 
 function checkOn(e){
-    const tooltip = getTooltipForElement(e.target);
-    if (!tooltip) return;
-    //console.log('enter', tooltip)
-    tooltip.showFor(e.target);
+    getTooltipForElement(e.target)?.showFor(e.target);
+    // const tooltip = getTooltipForElement(e.target);
+    // if (!tooltip) return;
+    // tooltip.showFor(e.target);
 }
 function checkOff(e){
-    const tooltip = getTooltipForElement(e.target);
-    if (!tooltip) return;
-    //console.log('leave', tooltip)
-    tooltip.hide();
+    getTooltipForElement(e.target)?.hide();
+    // const tooltip = getTooltipForElement(e.target);
+    // if (!tooltip) return;
+    // tooltip.hide();
 }
-
 
 function getTooltipForElement(el) {
     const id = el.getAttribute('aria-labeledby') || el.getAttribute('aria-describedby');
