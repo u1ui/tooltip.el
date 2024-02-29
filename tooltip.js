13d9b680f9ccf7b4589d6450f96f9e3e89598a5b
Tobias Buschor <tobias.buschor@shwups.ch>
2024-02-29 13:30:13 +0100

    Update Placer.js version in tooltip.js

--- tooltip.js
+++ tooltip.js
@@ -1,5 +1,5 @@
-//import {Placer} from 'https://cdn.jsdelivr.net/gh/u1ui/Placer.js@3.6.0/Placer.js';
-import {Placer} from '../Placer.js@3.6.0/Placer.js';
+//import {Placer} from 'https://cdn.jsdelivr.net/gh/u1ui/Placer.js@4.0.0/Placer.js';
+import {Placer} from '../Placer.js@4.0.0/Placer.js';
 
 let idCounter = 0;
 
